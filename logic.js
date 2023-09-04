3d893bb03d51c095a9e1f53a485d351d7711474d
Luis <141294557+LuisECV0@users.noreply.github.com>
2023-09-03 19:23:42 -0500

    se añadio la parte logica de la calculadora

--- logic.js
+++ logic.js
@@ -1,1 +1,17 @@
 
+let displayValue = '';
+//captura de datos
+function appendToDisplay(value) {
+    displayValue = displayValue + value;
+    document.getElementById('display').value = displayValue;
+}
+//borrar todo
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = displayValue;
+}
+//obtener resultados
+function calculateResult() {
+        displayValue = eval(displayValue);
+        document.getElementById('display').value = displayValue;
+    }
